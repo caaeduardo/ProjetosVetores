78a37b7bb97f197e00f395a0ff1e1b0c2692b4de
Carlos E. (𝕮𝖆𝕯𝖚) <carloseduardosilva419@gmail.com>
2024-10-14 22:13:41 -0300

    Primeira Versão do Projeto Vetores

---
+++ Lista1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosVetores
+{
+    internal class Lista1
+    {
+        internal void Exe1()
+        {
+            Console.WriteLine("1. Média Aritmética: Crie um programa que leia um vetor de 10 números reais e calcule a média aritmética.\n");
+
+            {
+                double[] numeros = new double[10];
+                double soma = 0;
+
+                Console.WriteLine("Digite 10 números reais:");
+                for (int i = 0; i < 10; i++)
+                {
+                    numeros[i] = double.Parse(Console.ReadLine());
+                    soma += numeros[i];
+                }
+
+                double media = soma / 10;
+                Console.WriteLine("A média aritmética é: " + media);
+
+
+            }
+            Console.WriteLine("Fim do exercício 1");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+        internal void Exe2()
+        {
+            Console.WriteLine("2. Ordenação Alfabética: Faça um programa que leia um vetor de 5 nomes e os imprima em ordem alfabética.\n");
+
+            {
+
+                string[]  nomes = new string[5];
+                Console.WriteLine("Digite 5 nomes: ");
+                for
+                    (int i = 0;i < 5;i++)
+                {
+                    nomes[i] = Console.ReadLine();
+                }
+                Array.Sort(nomes);
+                Console.WriteLine();
+                foreach 
+                    (string nome in nomes)
+                {
+                    Console.WriteLine(nome);
+                }
+
+            }
+            Console.WriteLine("Fim do exercício 2");
+            Console.WriteLine("-------------------------------------------------------------");
+
+        }
+
+        internal void Exe3()
+        {
+            Console.WriteLine("3. 3. Números Repetidos: Crie um programa que leia um vetor de 10 números inteiros e verifique se existe algum número repetido.\n ");
+
+            {
+
+
+            }
+            Console.WriteLine("Fim do exercício 3");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+            
+        internal void Exe4() { }
+        internal void Exe5() { }
+        internal void Exe6() { }
+        internal void Exe7() { }
+        internal void Exe8() { }
+        internal void Exe9() { }
+        internal void Exe10() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosVetores
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string Consultar;
+            do
+            {
+
+                menulistas();
+                Console.WriteLine("Por gentileza, deseja continuar as atividades de Vetores??? (S/N):\n");
+                Consultar = Console.ReadLine().ToUpper();
+
+
+            }
+
+            while (Consultar == "S");
+
+        
+
+
+        }
+
+        private static void menulistas()//metodo
+
+
+        {
+            
+
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Clear();
+            int op = 0;
+            Console.WriteLine("############# VETORES ##############\nCadu 1.2 Version MOD | Caaeduardo      ");
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.WriteLine("1 - PRIMEIRA LISTA DOS VETORES");
+            Console.WriteLine("0 - Para Fechar o Shell\n");
+
+            Console.Write("Numero da LISTA: ");
+            op = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("-------------------------------------------------------------");
+
+
+            //Switch Case
+            switch (op)
+            {
+                case 0:
+                    // Use o comando abaixo para interromper o programa
+                    Environment.Exit(0);
+                    break;
+
+
+                //LISTA 1
+
+                case 1:   //Menu da primeira Lista de atividades1
+                    Lista1 lista1 = new Lista1();
+                    Console.Clear();
+                    int opLista1 = 0;
+                    Console.WriteLine("############# VETORES (LISTA 1) ##############");
+                    Console.WriteLine("-------------------------------------------------------------");
+                    Console.WriteLine("1 - Primeira atividade ");
+                    Console.WriteLine("2 - Segunda atividade");
+                    Console.WriteLine("3 - Terceira  atividade");
+                    Console.WriteLine("4 - Quarta  atividade");
+                    Console.WriteLine("5 - Quinta  atividade");
+                    Console.WriteLine("6 - Sexta  atividade");
+                    Console.WriteLine("7 - Sétima  atividade");
+                    Console.WriteLine("8 - Oitava  atividade");
+                    Console.WriteLine("9 - Nona  atividade");
+                    Console.WriteLine("10 - La Décima  atividade");
+                    Console.WriteLine("Digite o numero da atividade: \n");
+                    opLista1 = int.Parse(Console.ReadLine());
+
+                    switch (opLista1)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe1();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe2();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe3();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe4();
+                            break;
+                        case 5:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe5();
+                            break;
+                        case 6:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe6();
+                            break;
+                        case 7:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe7();
+                            break;
+                        case 8:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe8();
+                            break;
+                        case 9:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe9();
+                            break;
+                        case 10:
+                            Console.Clear();
+                            Console.WriteLine("-------------------------------------------------------------");
+                            lista1.Exe10();
+                            break;
+                        default:
+                            Console.WriteLine("Tente outro item da lista");
+                            break;
+
+                    }
+                    break;
+            }
+        }
+    
+    }
+
+
+
+}
